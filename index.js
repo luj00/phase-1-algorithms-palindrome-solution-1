6d910aec1a6bd6fed5cc4e6068c52965341a6dc5
root <root@DESKTOP-598GE48.localdomain>
2023-10-16 15:14:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+  function reverseString(word) {
+    // create an array from the input string
+    const wordArray = word.split("");
+    // reverse the array
+    const reversedWordArray = wordArray.reverse();
+    // create a string from the reversed array
+    const reversedWord = reversedWordArray.join("");
+    // return the reversed string
+    return reversedWord;
+  }
+  
+  function isPalindrome(word) {
+    // reverse the input string
+    const reversedWord = reverseString(word);
+    // if the reversed string is the same as the input
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
